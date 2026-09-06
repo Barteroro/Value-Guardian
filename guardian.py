529dc786202bf8e62f59269c1a0bf9c33a5d6ef7
Bartero <333bbartek@gmail.com>
2026-09-06 18:22:44 +0200

    Usuwa przy 60 wartościach

--- guardian.py
+++ guardian.py
@@ -4,34 +4,48 @@ import os
 
 
 URL = "https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=ZLOTO"
-selektor = "#quotes-profile-header-box > div.o-quotes-profile-header-box__top > div.o-quotes-profile-header-box__numbers > div.o-quotes-profile-header-box__data > div.o-quotes-profile-header-box__price > span"
 
 HEADERS= {
      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
+
+selektor = "#quotes-profile-header-box > div.o-quotes-profile-header-box__top > div.o-quotes-profile-header-box__numbers > div.o-quotes-profile-header-box__data > div.o-quotes-profile-header-box__price > span"
+
+
 response = requests.get(URL, headers = HEADERS)
 soup = BeautifulSoup(response.text, "html.parser")
 cena =soup.select_one(selektor)
+
 if cena:
-    cena_sama = cena.text.strip("USD")
-    cena_sama = cena_sama.replace("\xa0", "")
-    cena_sama = int(round(float((cena_sama.replace(" ", "")).replace(",", "."))))
-    print("Bot odczytał cene: {}".format(cena_sama))
+    cena_nowa = cena.text.replace("USD", "")
+    cena_nowa = cena_nowa.replace("\xa0", "")
+    cena_nowa = int(round(float((cena_nowa.replace(" ", "")).replace(",", "."))))
+    print("Bot odczytał cene: {}".format(cena_nowa))
 else:
     print("Bot nic nie zlalazł, gg")
+
+linie = []
 if os.path.exists("dane.txt") and os.path.getsize("dane.txt") > 0:
     with open("dane.txt", "r") as Tr:
-        odczyt = Tr.readlines()
-        najstarsze = int(odczyt[0].strip())
+        linie = [linia.strip() for linia in Tr.readlines() if linia.strip()]
+if linie:
+    
+        najstarsze = int(linie[0])
         spadek = najstarsze * 0.8
-        if cena_sama <= spadek:
-            print("Cena spadłą, najnowsza {}, najstarsza {}".format(cena_sama, najstarsze))
+        
+        if cena_nowa <= spadek:
+            print("Cena spadła, najnowsza {}, najstarsza {}".format(cena_nowa, najstarsze))
         else:
             pass
+
 else:
-    print("To pierwsze uruchomienie")
-with open("dane.txt", "a") as Ta:
-    Ta.write("{}\n".format(cena_sama))
+    pass
+linie.append(cena_nowa)
+if len(linie) > 60:
+    linie.pop(0)
+with open("dane.txt", "w") as Ta:
+    for i in linie:
+        Ta.write("{}\n".format(i))
     
 
     
