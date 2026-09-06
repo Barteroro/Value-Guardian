9f02e9248442c43205252e6e0f0c517c61bbe651
Bartero <333bbartek@gmail.com>
2026-09-06 19:36:47 +0200

    Gotowy Golduch

--- guardian.py
+++ guardian.py
@@ -4,7 +4,7 @@ import os
 
 
 URL = "https://www.bankier.pl/inwestowanie/profile/quote.html?symbol=ZLOTO"
-
+DISCORD_WEEBHOOK = "https://discord.com/api/webhooks/1546203996367360210/DPQ5sema5EU6HScUC7DxzxOVbEOOzv9VFHg7wNcvKNOK1GHGigFqAOlz1J1u5l_7Ep-A"
 HEADERS= {
      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
 }
@@ -29,12 +29,18 @@ if os.path.exists("dane.txt") and os.path.getsize("dane.txt") > 0:
     with open("dane.txt", "r") as Tr:
         linie = [linia.strip() for linia in Tr.readlines() if linia.strip()]
 if linie:
-    
         najstarsze = int(linie[0])
         spadek = najstarsze * 0.8
         
         if cena_nowa <= spadek:
-            print("Cena spadła, najnowsza {}, najstarsza {}".format(cena_nowa, najstarsze))
+            discord = {"content": "Cena spadła, najnowsza {}, najstarsza {}".format(cena_nowa, najstarsze)}
+            try:
+                requests.post(DISCORD_WEEBHOOK, json = discord)
+                print("Wysłało powiadomienie")
+            except Exception as e:
+                print("NIE wysłało powiadomienia {}".format(e))
+
+
         else:
             pass
 
